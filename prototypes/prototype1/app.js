52c1db653ddb06789ceaa065c732d0eaad49394d
redahm <hammada.reda.dev@gmail.com>
2021-12-20 13:08:48 +0100

    update

---
+++ prototypes/prototype1/app.js
@@ -0,0 +1,25 @@
+
+document.getElementById('equal').addEventListener('click', function  result(){
+    alert('result');
+    restInput();
+
+
+});
+
+
+function insert(val){
+  document.getElementById('result-bar').value+= val;
+
+}
+
+function takeNumbers(){
+
+
+}
+
+function restInput(){
+
+
+    document.getElementById('result-bar').value = " ";
+}
+
