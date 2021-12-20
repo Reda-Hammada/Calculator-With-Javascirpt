480baaadec91aa653051a70c25254658ccde30c9
reda hammada <hammada.reda.dev@gmail.com>
2021-12-20 22:34:27 +0100

    update

--- prototypes/prototype1/app.js
+++ prototypes/prototype1/app.js
@@ -2,3 +2,21 @@
 let num1, num2, solution, operation;
 let num1String, num2String;
 
+//Métier
+function Calculation(num1, num2, operation){
+  let _solution = undefined;
+    switch(operation){
+
+      case '+':
+        _solution = num1 + num2;
+        break;
+      case '-':
+        _solution = num1 - num2;
+        break;
+
+      default: 
+        break;
+    }
+
+    return _solution;
+}
