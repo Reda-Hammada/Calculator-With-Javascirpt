aea9fb8c1e679ed641ad948d2849c029fa67a0e8
redahm <hammada.reda.dev@gmail.com>
2021-12-21 12:33:14 +0100

    update

--- prototypes/prototype1/app.js
+++ prototypes/prototype1/app.js
@@ -34,11 +34,55 @@ function ClickNumber(number){
 
       num2String += number;
     }
-  }
+
+    result();
+   
 }
 
-function Afficher(number){
+function result(number){
 
   let resultInput  = document.getElementById("resultInput");
   resultInput.value = " ";
+
+  if(num1 != undefined && num2 != undefined && operation != undefined){
+
+    resultInput.value = number;
+}else{
+    if(num1String != undefined)
+    afficheur.value += xString 
+    if(operation != undefined)
+        resultInput.value += operation     
+    if(num2String != undefined)
+        resultInput.value += yString 
+
+    }
+
+}
+
+function Operation(operationParam){
+  if(operation == undefined){
+      operation = operationParam;
+      num1 = parseFloat(num1String);
+      result();
+  }else{
+      alert("Vous avez déjà choisi l'opération " + operation);
+  }
+}
+
+function Egale(){
+  num1 = parseFloat(num1String);
+  num2 = parseFloat(num2String);
+  solution = Calculer(num1,num2,operation);
+  result(solution);
+}
+
+function Init(){
+  num1 = undefined;
+  num2 = undefined;
+  num1String = undefined;
+  num2String = undefined;
+  operation = undefined;
+  let resultInput = document.getElementById("resultInput");
+  
+  resultInput.value = "";
 }
