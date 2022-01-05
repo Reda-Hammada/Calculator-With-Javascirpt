b4e54072b11eb1b5cb51652112446b5c2405268b
redahm <hammada.reda.dev@gmail.com>
2022-01-05 13:27:59 +0100

    calculator class added

---
+++ prototypes/prototype2/oop.js
@@ -0,0 +1,60 @@
+class calculatrice{
+    #_num1;
+    #_num2;
+    #_result;
+    #_operation;
+
+    get num1(){
+        return this.#_num1;
+    }
+
+    set num1(value){
+        this.#_num1 = value;
+    }
+
+    get num2(){
+        return this.#_num2;
+    }
+
+    set num2(value){
+
+        this.#_num2 = value;
+    }
+
+    get operation(){
+        return this.#_operation;
+    }
+
+    set operation(value){
+        this.#_operation = value
+    }
+
+    get result(){
+        return this.#_result;
+    }
+
+    set result(value){
+        this.#_result = value;
+    }
+
+    calculate(){
+        this.#_reuslt = undefined;
+        switch(this.#_operation){
+            case '+':
+                this.#_result = this.#_num1 + this.#_num2;
+                break;
+            case '-':
+                this.#_result = this.#_num1 - this.#_num2;
+                break;
+        }
+
+        return this.#_result;
+    }
+
+    reset(){
+        this.#_num1 = undefined;
+        this.#_num2 = undefined; 
+        this.#_result = undefined;
+        this.#_operation = undefined;
+    }
+}
